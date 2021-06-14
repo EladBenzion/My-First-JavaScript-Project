df78f55c439ee57c2aba13a454942a9814617454
Elad Benzion <85366496+EladBenzion@users.noreply.github.com>
2021-06-14 15:50:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,6 @@ button.addEventListener('click', async (event) => {
     score.innerHTML = numberOfClicks;
     numberOfClicks++;
 })
-// function removeNumber () {
-//     const loop = setInterval(() => {
-//         score.innerHTML = numberOfClicks;
-//         numberOfClicks--;
-//     }, 1000);
-//     return loop;
-// }
 
 if(Number(score.innerHTML) == 20) {
     score.innerHTML = numberOfClicks;
